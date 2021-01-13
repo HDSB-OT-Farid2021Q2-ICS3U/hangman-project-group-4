cfcad0aae8bd276a1c2d047a8c257496fc994260
Evan <74550800+Evant04@users.noreply.github.com>
2021-01-13 12:02:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #the manin file
+import wordlist
+
+x = (wordlist.words)
+
+makelist = x.split('\n')
+
+
+def head():
+    print ('  (o)')
+
+def body():
+    print(' |[]|')
+
+def legs():
+    print('  LL')
+
+print('''  |
+  |
+  |''')
+head()
+body()
+legs()
