b75fbb0e336195b04245b6a9550156fca238e8bd
fkasperba <74547347+fkasperb@users.noreply.github.com>
2021-01-15 13:32:14 -0500

    hangman lives

--- main.py
+++ main.py
@@ -10,3 +10,53 @@ def hideword(word, guessedlett):
         else :
             lisofwords.append('_')
     print(''.join(lisofwords))
+
+    
+    def lives(lives)
+    if lives == 1
+        print("  ____   ")
+        print(" |    |  ")
+        print(" |       ")
+        print(" |       ")
+        print(" |       ")
+        print("_|___    ")
+        
+    elif lives == 2
+        print("  ____   ")
+        print(" |    |  ")
+        print(" |    O  ")
+        print(" |       ")
+        print(" |       ")
+        print("_|___    ")
+
+    elif lives == 3
+        print("  ____   ")
+        print(" |    |  ")
+        print(" |   _O_ ")
+        print(" |       ")
+        print(" |       ")
+        print("_|___    ")
+
+    elif lives == 4
+        print("  ____   ")
+        print(" |    |  ")
+        print(" |   _O_ ")
+        print(" |    |  ")
+        print(" |       ")
+        print("_|___    ")
+
+    elif lives == 5
+        print("  ____   ")
+        print(" |    |  ")
+        print(" |   _O_ ")
+        print(" |    |  ")
+        print(" |   /   ")
+        print("_|___    ")
+
+    elif lives == 6
+        print("  ____   ")
+        print(" |    |  ")
+        print(" |   _O_ ")
+        print(" |    |  ")
+        print(" |   / \ ")
+        print("_|___    ")
