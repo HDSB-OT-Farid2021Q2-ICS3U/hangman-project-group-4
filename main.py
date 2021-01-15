96a630d5933162019dc1b584c68001ffd71016a7
fkasperba <74547347+fkasperb@users.noreply.github.com>
2021-01-15 13:16:11 -0500

    shorten it

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-#the manin file
-
-
-
 def hideword(word, guessedlett):
     lisofwords = []
     guessedlet = [' ']
     sep = ''
-
-    for x in guessedlett :
-        guessedlet.append(x)
-
-    
+    guessedlet.extend(guessedlett)
     for i in word:
 
         if i in guessedlet :
             lisofwords.append(i)
         else :
             lisofwords.append('_')
-
-
     print(''.join(lisofwords))
-
