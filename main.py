f1b7d9e5e9f11299cf0ed45a832555e00c94dcd4
fkasperba <74547347+fkasperb@users.noreply.github.com>
2021-01-15 13:09:39 -0500

    hideword thing

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #the manin file
+
+
+
+def hideword(word, guessedlett):
+    lisofwords = []
+    guessedlet = [' ']
+    sep = ''
+
+    for x in guessedlett :
+        guessedlet.append(x)
+
+    
+    for i in word:
+
+        if i in guessedlet :
+            lisofwords.append(i)
+        else :
+            lisofwords.append('_')
+
+
+    print(','.join(lisofwords))
+
